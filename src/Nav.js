5a3a784b230ca33ec82bdea2649cf51ca9df4be3
ravi <69068706+Rvirao@users.noreply.github.com>
2022-03-18 20:15:28 +0530

    buttons modified

--- src/Nav.js
+++ src/Nav.js
@@ -13,8 +13,8 @@ function Nav() {
       <li className="cursor-pointer">COMMUNITY</li>
     </ul>
     <div className=" py-1 flex space-x-4 md:text-xs xl:text-base ">
-    <button className="px-4 py-1 border-2 rounded-full text-red hover:text-black-50 font-bold border-red hover:border-black-50">CREATE GAME</button>
-    <button className="px-4 py-1 border-2 rounded-full text-white bg-blue-50  hover:bg-white font-bold focus:ring-4 hover:border-black-100 hover:text-black-100 ">CONNECT WALLET</button></div>
+    <button className="px-1 sm:px-2 lg:text-base sm:text-lg text-sm py-1 border-2 rounded-full text-red hover:text-black-50 font-bold border-red hover:border-black-50">CREATE GAME</button>
+    <button className="px-1 sm:px-2 lg:text-base sm:text-lg text-sm py-1 border-2 rounded-full text-white bg-blue-50  hover:bg-white font-bold focus:ring-4 hover:border-black-100 hover:text-black-100 ">CONNECT WALLET</button></div>
   </nav>
   )
 }
